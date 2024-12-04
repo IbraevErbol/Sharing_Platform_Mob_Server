6fe03e143852b535bb279c5b5770993dac45a068
IbraevErbol <129647559+IbraevErbol@users.noreply.github.com>
2024-12-05 00:22:32 +0500

    s

--- Middleware/uploadsMiddleware.js
+++ Middleware/uploadsMiddleware.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 import multerStorageCloudinary from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary';
+import cloudinary from '../config/cloudinary.js';
 
 const storage = multerStorageCloudinary({
   cloudinary: cloudinary,  // Используем настроенную библиотеку cloudinary
