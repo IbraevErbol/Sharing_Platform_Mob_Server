1458f87d4756714fa447348621b6f705d2e52021
IbraevErbol <129647559+IbraevErbol@users.noreply.github.com>
2024-12-05 00:39:26 +0500

    Update uploadsMiddleware.js

--- Middleware/uploadsMiddleware.js
+++ Middleware/uploadsMiddleware.js
@@ -1,29 +1,16 @@
 import multer from 'multer';
 import cloudinary from '../config/cloudinary.js';
-import multerStorageCloudinary from 'multer-storage-cloudinary';
+import {CloudinaryStorage} from 'multer-storage-cloudinary';
 
-const storage = multerStorageCloudinary({
+const storage = new CloudinaryStorage({
   cloudinary: cloudinary,  // Ваши настройки Cloudinary
   folder: 'uploads',       // Указываем папку в Cloudinary
   allowedFormats: ['jpeg', 'jpg', 'png'],  // Разрешенные форматы
   transformation: [{ quality: 'auto', fetch_format: 'auto' }],
 });
 
-const fileFilter = (req, file, cb) => {
-  const filetypes = /jpeg|jpg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    cb(new Error('Только изображения формата JPEG, PNG или JPG'));
-  }
-};
-
 const upload = multer({
   storage,
-  fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
